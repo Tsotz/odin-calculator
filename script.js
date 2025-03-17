0401fd9384daa59688878579041e60dcc95944c7
Tsotz <201083717+Tsotz@users.noreply.github.com>
2025-03-17 16:51:02 +0300

    basic fucntions done

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function (num1, num2) { return num1 + num2;};
+
+const subtract = function (num1, num2) {return num1 - num2;};
+
+const multiply = function (num1, num2) {return num1 * num2;};
+
+const divide = function (num1, num2) {return num1 / num2;};
+
+let firstNum ;
+let secondNum ;
+let operator ;
+
+console.log(add(2,9));
+console.log(subtract(2,9));
+console.log(multiply(2,9));
+console.log(divide(2,9))
