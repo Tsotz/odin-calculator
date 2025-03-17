db0ec3bcc807a99a69f6877c603c293d87d6ea82
Tsotz <201083717+Tsotz@users.noreply.github.com>
2025-03-17 17:45:17 +0300

    buttons called and display functional

--- script.js
+++ script.js
@@ -10,6 +10,36 @@ let firstNum ;
 let secondNum ;
 let operator ;
 
+
+const operate = function (operator, firstNum, secondNum) {
+
+    return operator(firstNum, secondNum);
+
+}
+
+const buttons = document.querySelectorAll("button")
+
+let pressedButton;
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        
+       pressedButton =  displayF(button.textContent);
+        return pressedButton;
+    }); 
+});
+
+
+
+
+
+const display = document.querySelector("#display");
+display.style.backgroundColor = "black";
+display.style.color = "white";
+
+const displayF = function(input) {display.textContent = input};
+
+
 console.log(add(2,9));
 console.log(subtract(2,9));
 console.log(multiply(2,9));
